327b14429ad8989b83895ca11cb40e6f8e0d2717
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-02-03 22:30:41 +0530

    feat: added header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let headerTitle = 'default title'
+</script>
+
+<main>
+  <h1>{headerTitle}</h1>
+</main>
+
+<style>
+  * {
+    /* background-color: red; */
+    font-weight: 400;
+  }
+</style>
